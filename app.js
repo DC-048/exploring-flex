d6a54a1781be0c3821b202bd63aa68c02b479c2b
DC-048 <113233491+DC-048@users.noreply.github.com>
2022-09-10 13:30:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log(`Hey`);
+
+function justifyContentCenter() {
+  document.getElementById("area-display-1").style.justifyContent = "center";
+}
+
+function justifyContentStart() {
+  document.getElementById("area-display-1").style.justifyContent = "start";
+}
+
+function justifyContentEnd() {
+  document.getElementById("area-display-1").style.justifyContent = "end";
+}
+
+function justifyContentSpaceAround() {
+  document.getElementById("area-display-1").style.justifyContent =
+    "space-around";
+}
+
+function justifyContentSpaceBetween() {
+  document.getElementById("area-display-1").style.justifyContent =
+    "space-between";
+}
+
+function justifyContentSpaceEvenly() {
+  document.getElementById("area-display-1").style.justifyContent =
+    "space-evenly";
+}
+
+function alignItemsCenter() {
+  document.getElementById("area-display-1").style.alignItems = "center";
+}
+
+function alignItemsStart() {
+  document.getElementById("area-display-1").style.alignItems = "flex-start";
+}
+
+function alignItemsEnd() {
+  document.getElementById("area-display-1").style.alignItems = "flex-end";
+}
+
+function alignItemsStretch() {
+  document.getElementById("area-display-1").style.alignItems = "stretch";
+}
+
+function alignItemsBaseline() {
+  document.getElementById("area-display-1").style.alignItems = "baseline";
+}
+
+/* function myFunction() {
+  document.getElementById("zxc").style.color = "blue";
+  // document.getElementById("zxc").style.visibility = "hidden";
+  //document.getElementById("zxc").style.alignItems = "flex-end";
+  document.getElementById("qwe").style.alignItems = "center";
+}
+*/
